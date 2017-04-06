478dc004fc652e5109b0dc9031fc7745f279ca4f
Aleksandr Anokhin <anohin@newtonideas.com>
2017-04-06 15:05:32 +0300

    Add examples

--- examples/hello.py
+++ examples/hello.py
@@ -1,1 +1,23 @@
-print "FOO"
+def diff(first, second):
+  intersection = [k for k in first if k in second]
+  addition = [k for k in second if k not in first]
+  deletion = [k for k in first if k not in second]
+
+  return intersection, addition, deletion
+    
+
+first = {'lorem': 1, 'amet': 5}
+second = {'ipsum': 2, 'dolor': 3, 'sit': 4}
+    
+result = diff(first, second)
+
+_, added, removed = result
+
+print 'diffed dicts'
+print first, second
+
+print 'added keys:'
+print added
+
+print 'removed keys:'
+print removed
--- examples/hello.rb
+++ examples/hello.rb
@@ -1,1 +1,9 @@
-puts "HEY"
+def fib_r(a, b, n)
+  n == 0 ? a : fib_r(b, a + b, n - 1)
+end
+
+def fib(n)
+  fib_r(0, 1, n)
+end
+
+puts (0..10).map{ |n| fib(n) }
